7453d39eed7c6f62df7a83672516d07399a0b82a
soheil behzad <soheil_dark999@yahoo.com>
2019-07-09 13:33:56 -0700

    created models files category, product, transaction js

---
+++ models/product.js
@@ -0,0 +1,23 @@
+module.exports = (sequelize, DataTypes) => {
+  const Product = sequelize.define("Product", {
+    name: DataTypes.STRING,
+    quantity: DataTypes.INTEGER,
+    image: DataTypes.STRING,
+    details: DataTypes.TEXT,
+    price: DataTypes.INTEGER
+  });
+
+  Product.associate = function(models) {
+    Product.belongsTo(models.Category, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+    Product.associate = models => {
+      Product.hasMany(models.Transaction);
+    };
+  
+  };
+  return Product;
+};
