42e92e0b961d7a7fed993db3d3fad7f7435ea4ff
Marina Delkova-Moro <marinadelkovamoro@gmail.com>
2019-07-09 14:41:35 -0700

    added database seed file with products

--- models/transaction.js
+++ models/transaction.js
@@ -1,22 +1,22 @@
 module.exports = (sequelize, DataTypes) => {
-    const Transaction = sequelize.define("Transaction", {
-        numberofItems: DataTypes.INTEGER,
-        total: DataTypes.INTEGER,
-        paymentType: DataTypes.INTEGER
+  const Transaction = sequelize.define("Transaction", {
+    numberofItems: DataTypes.INTEGER,
+    total: DataTypes.INTEGER,
+    paymentType: DataTypes.INTEGER
+  });
+
+  Transaction.associate = function(models) {
+    Transaction.belongsTo(models.User, {
+      foreignKey: {
+        aloowNull: false
+      }
     });
+    Transaction.belongsTo(models.Product, {
+      foreignKey: {
+        aloowNull: false
+      }
+    });
+  };
 
-    Transaction.associate = function (models) {
-        Transaction.belongsTo(models.User, {
-            foreignKey: {
-                aloowNull: false
-            }
-        });
-        Transaction.belongsTo(models.Product, {
-            foreignKey: {
-                aloowNull: false
-            }
-        });
-    };
-
-    return Transaction;
+  return Transaction;
 };
