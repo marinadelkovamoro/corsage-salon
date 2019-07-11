7c38e482ca1863067d76810799a73d4a95113ded
J Frost <icefang@hotmail.com>
2019-07-11 13:16:11 -0700

    getting categories from database

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,7 +3,7 @@ const isAuthenticated = require("../config/middleware/isAuthenticated");
 module.exports = app => {
   // Load signup page
   app.get("/", (req, res) => {
-    db.Category.findAll({}).then(function (catData) {
+    db.Category.findAll({}).then(function(catData) {
       var data = { categories: catData };
       if (req.user) {
         data.user = req.user.name;
@@ -13,21 +13,31 @@ module.exports = app => {
   });
 
   // Load login page
-  app.get("/login", (req, res) => res.render("login", { islogin: true, layout: 'blank' }));
+  app.get("/login", (req, res) =>
+    res.render("login", { islogin: true, layout: "blank" })
+  );
 
   // Load Signup page
-  app.get("/signup", (req, res) => res.render("login", { islogin: false, layout: 'blank' }));
+  app.get("/signup", (req, res) =>
+    res.render("login", { islogin: false, layout: "blank" })
+  );
 
   // get all product by id
-  app.get("/products/:id", function (req, res) {
-    db.Product.findAll({
-      where: {
-        CategoryId: req.params.id
+  app.get("/products/:id", function(req, res) {
+    db.Category.findAll({}).then(function(catData) {
+      var data = { categories: catData };
+      if (req.user) {
+        data.user = req.user.name;
       }
-    }).then(function (dbProduct) {
-      // console.log(dbProduct);
-      res.render("products", {
-        products: dbProduct
+
+      db.Product.findAll({
+        where: {
+          CategoryId: req.params.id
+        }
+      }).then(function(dbProduct) {
+        data.products = dbProduct;
+
+        res.render("products", data);
       });
     });
   });
