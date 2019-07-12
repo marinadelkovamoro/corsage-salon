b667ede8ae7c8cc66d67af03355f7ed80a743f3a
J Frost <icefang@hotmail.com>
2019-07-12 10:06:17 -0700

    fix background, add shopping cart

--- public/js/shoppingcart.js
+++ public/js/shoppingcart.js
@@ -1,20 +1,46 @@
-// retrieve shopping cart form local storage;
 var myCart = JSON.parse(localStorage.getItem("mycart"));
 if (!myCart) {
   myCart = [];
 }
 
-function handlePurchaseButton(e) {
-  e.preventDefault();
+$("#cart-btn-checkout").on("click", function(event) {
+  // save to database. then display confirmation page.
+});
 
-   var transaction = {
-    id: parseInt($(this).attr("productid")),
-    name: $(this).parent().children("h2.card-title").text(),
-    price: parseInt($(this).parent().children("p.price").attr("value"))
-  };
-  myCart.push(transaction);
-  localStorage.setItem("mycart", JSON.stringify(myCart));
+$("#cart-btn-continue").on("click", function(event) {
+  window.location.replace("/");
+});
+
+function getCart() {
+  if (!document.getElementById("cart-page")) {
+    return;
+  }
+
+  var newRow;
+  var pName, pImg, pPrice, pUnits, pDelete;
+  var btnDelete, imgElem;
+
+  var total = 0;
+  var cartPage = $("#cart-page");
+  for (var i = 0; i < myCart.length; i++) {
+    total += myCart[i].price * myCart[i].numItems;
+    newRow = $("<div>").addClass("row").attr("item-id", myCart[i].id);
+    pName = $("<div>").addClass("col cart-item-name").text(myCart[i].name);
+
+    imgElem = $("<img>").addClass("cart-item-image img-fluid").attr("src", myCart[i].image);
+    pImg = $("<div>").addClass("col").append(imgElem);
+
+    pPrice = $("<div>").addClass("col");
+    pPrice.append($("<p>").text("$" + myCart[i].price.toLocaleString()));
+    pPrice.append($("<p>").text("Bitcoins"));
+
+    pUnits = $("<div>").addClass("col").text(myCart[i].numItems);
+
+    btnDelete = $("<button>").addClass("btn-cart-delete").text("delete");
+    pDelete = $("<div>").addClass("col").append(btnDelete);
+
+    newRow.append(pName, pImg, pPrice, pUnits, pDelete);
+    cartPage.append(newRow, $("<hr>"));
+  }
+  $(".cart-total").text("Total $" + total.toLocaleString() + " / bitcoin?");
 }
-
-// Add event listeners to the submit and delete buttons
-$(".btn-purchase").on("click", handlePurchaseButton);
