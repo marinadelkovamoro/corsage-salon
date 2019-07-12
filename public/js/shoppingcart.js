e174a67ee4730ded84c74a438589cf3c94a5ebd2
J Frost <icefang@hotmail.com>
2019-07-11 18:00:49 -0700

    prepare to pull

--- public/js/shoppingcart.js
+++ public/js/shoppingcart.js
@@ -1,7 +1,20 @@
-
 // retrieve shopping cart form local storage;
 var myCart = JSON.parse(localStorage.getItem("mycart"));
 if (!myCart) {
-    myCart = {};
+  myCart = [];
 }
 
+function handlePurchaseButton(e) {
+  e.preventDefault();
+
+   var transaction = {
+    id: parseInt($(this).attr("productid")),
+    name: $(this).parent().children("h2.card-title").text(),
+    price: parseInt($(this).parent().children("p.price").attr("value"))
+  };
+  myCart.push(transaction);
+  localStorage.setItem("mycart", JSON.stringify(myCart));
+}
+
+// Add event listeners to the submit and delete buttons
+$(".btn-purchase").on("click", handlePurchaseButton);
